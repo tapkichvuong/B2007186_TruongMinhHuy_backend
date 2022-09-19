66cdfd694ed249459c1f16686469d551d2bf2954
Truong Minh Huy <huytruongminh44@gmail.com>
2022-09-19 18:06:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter  =require("./app/routes/contact_route")
+const apiError = require("./app/apiError")
 
 const app = express();
 
@@ -10,5 +11,17 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application!" });
 });
 app.use("/api/contacts", contactRouter);
+
+//handle 404
+app.use((req, res, next) =>{
+    return next(new apiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last
+appp.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internet Server Error",
+    });
+})
 
 module.exports = app;
